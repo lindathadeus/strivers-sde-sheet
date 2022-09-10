dd5a262d4d54e543a4ae5160ac109a42180461d1
linda <@gmail.com>
2022-09-10 15:21:13 +0530

    just placeholder

---
+++ days/1/set_matrix_zeroes.c
@@ -0,0 +1,15 @@
+/****************************************************************
+* File name     : set_matrix_zeroes.c
+* Purpose       :
+* Author        : Linda J N
+* Creation date : 10-09-2022
+* Last modified : Saturday 10 September 2022 03:21:05 PM
+****************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+
+  return 0;
+}
