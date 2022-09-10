f700de5b1aace6f2935f18c8eb9d23bd8c0b8ebf
linda <@gmail.com>
2022-09-10 16:37:27 +0530

    version 1

--- days/1/set_matrix_zeroes.c
+++ days/1/set_matrix_zeroes.c
@@ -1,6 +1,17 @@
 /****************************************************************
 * File name     : set_matrix_zeroes.c
-* Purpose       :
+* Purpose       : Given a matrix if an element in the matrix is 0
+                  then you will have to set its entire column and
+                  row to 0 and then return the matrix.
+
+                  Input: matrix = [[1,1,1],
+                                   [1,0,1],
+                                   [1,1,1]]
+
+                  Output: matrix = [[1,0,1],
+                                    [0,0,0],
+                                    [1,0,1]]
+
 * Author        : Linda J N
 * Creation date : 10-09-2022
 * Last modified : Saturday 10 September 2022 03:21:05 PM
@@ -9,7 +20,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]){
+void printm(int M[3][3]) {
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++)
+      printf("%d   ", M[i][j]);
+    printf("\n");
+  }
+}
+
+int main(int argc, char *argv[]) {
+  int M[3][3] = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+
+  printm(M);
+
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      if (M[i][j] == 0) {
+        for (int ii = 0; ii < 3; ii++) {
+          for (int jj = 0; jj < 3; jj++) {
+            M[i][jj] = -1;
+            M[ii][j] = -1;
+          }
+        }
+      }
+    }
+  }
+  printf("\n");
+
+  for (int i = 0; i < 3; i++)
+    for (int j = 0; j < 3; j++)
+      if (M[i][j] == -1) M[i][j] = 0;
+
+  printm(M);
 
   return 0;
 }
